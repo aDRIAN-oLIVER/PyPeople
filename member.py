1bd7fa2b5671fde2e49bc996990a74590707170a
Adrián Oliver <aoliverbelando@gmail.com>
2025-06-15 17:40:15 +0200

    anonimizo el código. Edicompor company.

--- member.py
+++ member.py
@@ -4,8 +4,8 @@ Member class. It is used for representing each member of the team.
 from datetime import datetime
 
 class Member:
-    def __init__(self, codname, name, surname1, surname2, edicom_init_date, team_init_date):
-        """edicom_init_date and team_init_date must be in format dd-mm-yyyy"""
+    def __init__(self, codname, name, surname1, surname2, company_init_date, team_init_date):
+        """company_init_date and team_init_date must be in format dd-mm-yyyy"""
         self.codname = codname
         self.name = name
         self.surname1 = surname1
@@ -15,10 +15,10 @@ class Member:
         allowed_format = "%d-%m-%Y"
 
         try:
-            self.edicom_init_date = datetime.strptime(edicom_init_date, allowed_format)
+            self.company_init_date = datetime.strptime(company_init_date, allowed_format)
         except ValueError:
-            self.edicom_init_date = None
-            print("error: edicom_init_date format is not valid. Registered as None.")
+            self.company_init_date = None
+            print("error: company_init_date format is not valid. Registered as None.")
 
         try:
             self.team_init_date = datetime.strptime(team_init_date, allowed_format)
@@ -27,14 +27,14 @@ class Member:
             print("error: team_init_date format is not valid. Registered as None.")
             
 
-    def get_edicom_seniority(self):
-        """Returns the seniority in edicom in months"""
-        if self.edicom_init_date is None:
-            print("error: edicom_init_date is not registered in this member.")
+    def get_company_seniority(self):
+        """Returns the seniority in company in months"""
+        if self.company_init_date is None:
+            print("error: company_init_date is not registered in this member.")
             return "?" 
 
         today = datetime.today()
-        seniority = today - self.edicom_init_date
+        seniority = today - self.company_init_date
         seniority_in_months = seniority.days // 30
         return seniority_in_months
 
@@ -56,6 +56,6 @@ class Member:
 if __name__=="__main__":
     maria = Member("mgarcia", "maría", "garcía", "alejo", "03-04-2022", "3-5-25")
     print(maria.codname, maria.name, sep=" => ")
-    print("Antigüedad edicom", maria.get_edicom_seniority())
+    print("Antigüedad company", maria.get_company_seniority())
     print("Antigüedad equipo", maria.get_team_seniority())
     print("Email", maria.get_email())

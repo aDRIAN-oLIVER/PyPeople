20de29b16ea6dd80d03a391d9e671f4192f00f47
Adrián Oliver <aoliverbelando@gmail.com>
2025-06-15 17:13:46 +0200

    I create member class

---
+++ member.py
@@ -0,0 +1,61 @@
+"""
+Member class. It is used for representing each member of the team.
+"""
+from datetime import datetime
+
+class Member:
+    def __init__(self, codname, name, surname1, surname2, edicom_init_date, team_init_date):
+        """edicom_init_date and team_init_date must be in format dd-mm-yyyy"""
+        self.codname = codname
+        self.name = name
+        self.surname1 = surname1
+        self.surname2 = surname2
+
+        # Dates require a specific format
+        allowed_format = "%d-%m-%Y"
+
+        try:
+            self.edicom_init_date = datetime.strptime(edicom_init_date, allowed_format)
+        except ValueError:
+            self.edicom_init_date = None
+            print("error: edicom_init_date format is not valid. Registered as None.")
+
+        try:
+            self.team_init_date = datetime.strptime(team_init_date, allowed_format)
+        except ValueError:
+            self.team_init_date = None
+            print("error: team_init_date format is not valid. Registered as None.")
+            
+
+    def get_edicom_seniority(self):
+        """Returns the seniority in edicom in months"""
+        if self.edicom_init_date is None:
+            print("error: edicom_init_date is not registered in this member.")
+            return "?" 
+
+        today = datetime.today()
+        seniority = today - self.edicom_init_date
+        seniority_in_months = seniority.days // 30
+        return seniority_in_months
+
+    def get_team_seniority(self):
+        """Returns the seniority in the team in months""" 
+        if self.team_init_date is None:
+            print("error: team_init_date is not registered in this member.")
+            return "?"
+
+        today = datetime.today()
+        seniority = today - self.team_init_date
+        seniority_in_months = seniority.days // 30
+        return seniority_in_months
+
+    def get_email(self):
+        """Returns the corporative email"""
+        return f"{self.codname}@corporative_email.com"
+
+if __name__=="__main__":
+    maria = Member("mgarcia", "maría", "garcía", "alejo", "03-04-2022", "3-5-25")
+    print(maria.codname, maria.name, sep=" => ")
+    print("Antigüedad edicom", maria.get_edicom_seniority())
+    print("Antigüedad equipo", maria.get_team_seniority())
+    print("Email", maria.get_email())
